22c789d0a6485ff69bb7b305cc449f5e31162b92
Abel Abaho <abahoabel07@gmail.com>
2026-06-30 14:20:31 +0300

    Create AddNote button as a reusable component

---
+++ frontend/src/components/AddNoteButton.tsx
@@ -0,0 +1,7 @@
+export const AddNoteButton = () => {
+  return (
+    <button className="flex cursor-pointer items-center justify-center rounded-full border border-amber-500 bg-amber-300 px-5 py-2 font-normal text-gray-900 shadow-lg hover:bg-amber-400 active:scale-98 active:bg-amber-500">
+      Add note
+    </button>
+  );
+};
